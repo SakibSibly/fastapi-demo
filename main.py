a153d2b001f5cb3bbb3e9db9c485dba20b1b861f
SakibSibly <sibly@duck.com>
2025-03-06 16:07:37 +0600

    Intezer file view function added

--- main.py
+++ main.py
@@ -5,3 +5,8 @@ app = FastAPI()
 @app.get("/info")
 async def details():
     return {"data": "FastAPI Implementation basics"}
+
+
+@app.get("/number/{number}")
+async def getNumber(number: int):
+    return {"data": f"You entered {number}"}
